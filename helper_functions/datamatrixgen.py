16299a90e9ced34adbd55023e48915bef15844c0
Jaroslav Kramar <jaroslav.kramar@datasentics.com>
2022-11-29 15:15:07 +0100

    datamatrix gen

--- helper_functions/datamatrixgen.py
+++ helper_functions/datamatrixgen.py
@@ -4,13 +4,20 @@ import treepoem
 
 def prepair_code(stage, line, stock, position):
     return (
-        stage + str(line).zfill(2) + "-" + str(stock).zfill(2) + "-" + str(position).zfill(2)
+        stage
+        + str(line).zfill(2)
+        + "-"
+        + str(stock).zfill(2)
+        + "-"
+        + str(position).zfill(2)
     )
 
-def get_datamatrix_from_string(code: str):
-        return treepoem.generate_barcode(barcode_type="datamatrix", data=code)
 
-def create_one_dmtx(stage: str, line: str, stock:str, position: str):
+def get_datamatrix_from_string(code: str):
+    return treepoem.generate_barcode(barcode_type="datamatrix", data=code)
+
+
+def create_one_dmtx(stage: str, line: str, stock: str, position: str):
     """_summary_
 
     Args:
@@ -27,6 +34,7 @@ def create_one_dmtx(stage: str, line: str, stock:str, position: str):
     image = get_datamatrix_from_string(code)
     image = image.convert("L")
     return image, code
+
 
 def create_one_dmtx_from_code(code: str):
     """_summary_
@@ -45,13 +53,13 @@ def create_one_dmtx_from_code(code: str):
     return image
 
 
-def create_one_dmtx_png(stage, line, stock, position, path: str="data/dmtx/"):
+def create_one_dmtx_png(stage, line, stock, position, path: str = "data/dmtx/"):
     image, code = create_one_dmtx(stage, line, stock, position)
     image.save(f"{path}{code}.png")
 
 
 def generate_for_all_images():
-    """ Gerneate all images for all possible combinations of stage, line, stock and position
+    """Gerneate all images for all possible combinations of stage, line, stock and position
     Args:
     Returns:
     Exceptions:
@@ -66,12 +74,15 @@ def generate_for_all_images():
                     image = get_datamatrix_from_string(code)
                     image.save(f"data/dmtx/{code}.png")
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-t", "--stage", type=str, default="F", help="Stage")
     parser.add_argument("-l", "--line", type=int, default=1, help="Line")
     parser.add_argument("-s", "--stock", type=int, default=1, help="Stock")
     parser.add_argument("-p", "--position", type=int, default=1, help="Position")
-    parser.add_argument("-o", "--output", type=str, default="data/dmtx/", help="Output path")
+    parser.add_argument(
+        "-o", "--output", type=str, default="data/dmtx/", help="Output path"
+    )
     args = parser.parse_args()
     create_one_dmtx_png(args.stage, args.line, args.stock, args.position, args.output)
